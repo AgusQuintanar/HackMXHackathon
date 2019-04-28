6bb606f8d7aaed3a978e183209910edcb52af7a4
AgusQuintanar <agusquintanar17@gmail.com>
2019-04-28 12:19:55 -0500

    Added comments and reestructure

--- Version1.0/main.py
+++ Version1.0/main.py
@@ -10,7 +10,7 @@ from sklearn.model_selection import train_test_split
 import sklearn.utils
 import re
 
-def arbol(palabras):
+def arbol(palabras): #Creacion de un arbol binario
     class nodo:
         """docstring for nodo"""
         def __init__(self, valor ,nivel):
@@ -22,17 +22,17 @@ def arbol(palabras):
     class Arbol:
         def __init__(self):
             self.raiz=None
-            self.limiteRiesgo=15
+            self.limiteRiesgo=10
             self.riesgoActual=0
             self.contadorPeligro = 0
 
-        def insertar(self,valor, nivel):
+        def insertar(self,valor, nivel): #Funcion para crear un nodo
             if self.raiz==None:
                 self.raiz=nodo(valor, nivel)
             else:
                 self._insertar(valor,self.raiz, nivel)
 
-        def _insertar(self,valor,nodoActual, nivel):
+        def _insertar(self,valor,nodoActual, nivel):#Funcion alternativa usada cuando la raiz ya tiene valor
             if valor<nodoActual.valor:
                 if nodoActual.nodoI==None:
                     nodoActual.nodoI=nodo(valor, nivel)
@@ -46,7 +46,7 @@ def arbol(palabras):
             else:
                 print("Valor repetido")			
         
-        def buscar(self,valor):
+        def buscar(self,valor):#Funcion  que busca un valor en cada nodo
             if self.raiz!=None:
                 return self._buscar(valor,self.raiz)			
             else:
@@ -67,19 +67,21 @@ def arbol(palabras):
             if self.raiz!=None:
                 self._imprimirArbol(self.raiz)
 
-        def _imprimirArbol(self,nodoActual):
+        def _imprimirArbol(self,nodoActual): #Funcion para probar que los nodos se insertaron correctamente
             if nodoActual!=None:
                 self._imprimirArbol(nodoActual.nodoI)
                 print (nodoActual.valor)
                 self._imprimirArbol(nodoActual.nodoD)	
 
-    arbol=Arbol()
-
+    arbol=Arbol() #Crea un objeto de tipo arbol
 
     diccionario = {"urgente":[5, "Ninguna institución bancaria utiliza este tipo de lenguaje alarmante."], "importante":[3, ""], "pago":[3, ""], "cargo":[5, "Verificar directamente con su banco este mismo."], "deuda":[4,""], "inmediatamente":[3, "Ninguna institución bancaria utiliza este tipo de lenguaje alarmante."],
                    "seguridad":[2, ""], "alerta":[2, ""],"intento":[1, ""],"contraseña":[3, "Ninguna institucion o agente bancario debe solicitarte este tipo de información."],"pin":[5, "Ninguna institucion o agente bancario debe solicitarte este tipo de información."],"cvv":[5, "Ninguna institucion o agente bancario debe solicitarte este tipo de información."],"bloqueado":[4, "Verificar directamente con tu banco dichas acciones."],
                    "bloqueada":[4, "Verificar directamente con tu banco dichas acciones."],"tarjeta":[3, ""],"premio":[4, "Desconfía de recompensas inesperadas o desconocidas."],"ganaste":[4, "Desconfía de recompensas inesperadas o desconocidas."],"loteria":[4, "Desconfía de recompensas inesperadas o desconocidas."],"valida":[2, ""],"cambie":[1,""],
                    "falló":[2, ""],"desorden":[1, ""],"contacto":[2, ""],"fraudulenta":[3, "Realiza una doble verificación directamente con tu banco, sobre movimientos sospechosos"]}
+
+    ### Diccionario que contiene las palabras claves para detectar phishing, incluye su valor de riesgo, asi como el mensaje 
+    ## que saldria si saliera un margen mayor a 15
 
     for palabra in diccionario:
         arbol.insertar(palabra, diccionario[palabra][0])
@@ -87,7 +89,7 @@ def arbol(palabras):
     for x in palabras:
         arbol.buscar(x)
 
-    if arbol.contadorPeligro > 0:
+    if arbol.contadorPeligro > 0: #Si obtiene un nivel de riesgo muy alto, se lanza una advertencia
         print('\nExiste una alta probabilidad de que este correo contenga Phishing')
         texto = ""
         for x in palabras:
@@ -105,11 +107,10 @@ def checarUrl(x_predict):
     num_good = data.label.value_counts()["good"]
     num_bad = data.label.value_counts()["bad"]
    
-
     #Shuffle del data set y verificar que la proporción es cercana a la orginal
 
     df = sklearn.utils.shuffle(data)
-    data = df[:300000] #Manejando solo 50000 variables para ser mas practicos
+    data = df[:50000] #Manejando solo 50000 variables para ser mas practicos
 
     num_good = df.label.value_counts()["good"]
     num_bad = df.label.value_counts()["bad"]
@@ -176,7 +177,7 @@ def leerEmail():
 def main():
     leer_email = leerEmail()
     arbol(leer_email[1])
-    if len(leer_email[0]) > 1:
+    if len(leer_email[0]) > 0:
         checarUrl(leer_email[0])
 
 main()
